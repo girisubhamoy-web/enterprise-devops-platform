1c1afc58a137fe42ded2c9c05e613fe124c3c71e
girisubhamoy-web <girisubhamoy@gmail.com>
2026-07-26 09:17:37 +0000

    Build enterprise VPC networking foundation

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -13,3 +13,72 @@ resource "aws_vpc" "this" {
   }
 
 }
+resource "aws_internet_gateway" "this" {
+
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.name}-igw"
+  }
+
+}
+resource "aws_subnet" "public_1" {
+
+  vpc_id = aws_vpc.this.id
+
+  cidr_block = var.public_subnet_1_cidr
+
+  availability_zone = "ap-south-1a"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.name}-public-1"
+  }
+
+}
+resource "aws_subnet" "public_2" {
+
+  vpc_id = aws_vpc.this.id
+
+  cidr_block = var.public_subnet_2_cidr
+
+  availability_zone = "ap-south-1b"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.name}-public-2"
+  }
+}
+resource "aws_subnet" "private_1" {
+
+  vpc_id = aws_vpc.this.id
+
+  cidr_block = var.private_subnet_1_cidr
+
+  availability_zone = "ap-south-1a"
+
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.name}-private-1"
+  }
+
+}
+
+resource "aws_subnet" "private_2" {
+
+  vpc_id = aws_vpc.this.id
+
+  cidr_block = var.private_subnet_2_cidr
+
+  availability_zone = "ap-south-1b"
+
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.name}-private-2"
+  }
+
+}
