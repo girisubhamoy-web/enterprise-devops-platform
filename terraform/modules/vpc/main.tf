6537e5b685e0b0fe6762dbde970f78316d962904
girisubhamoy-web <girisubhamoy@gmail.com>
2026-07-27 15:32:26 +0000

    Complete enterprise VPC networking

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -22,6 +22,58 @@ resource "aws_internet_gateway" "this" {
   }
 
 }
+resource "aws_eip" "nat" {
+
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.name}-nat-eip"
+  }
+
+}
+resource "aws_nat_gateway" "this" {
+
+  allocation_id = aws_eip.nat.id
+
+  subnet_id = aws_subnet.public_1.id
+
+  tags = {
+    Name = "${var.name}-nat"
+  }
+
+  depends_on = [
+    aws_internet_gateway.this
+  ]
+
+}
+resource "aws_route_table" "public" {
+
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.name}-public-rt"
+  }
+
+}
+resource "aws_route_table_association" "public_1" {
+
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+
+}
+
+resource "aws_route_table_association" "public_2" {
+
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+
+}
+
 resource "aws_subnet" "public_1" {
 
   vpc_id = aws_vpc.this.id
@@ -51,6 +103,34 @@ resource "aws_subnet" "public_2" {
     Name = "${var.name}-public-2"
   }
 }
+resource "aws_route_table" "private" {
+
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.name}-private-rt"
+  }
+
+}
+resource "aws_route_table_association" "private_1" {
+
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private.id
+
+}
+
+resource "aws_route_table_association" "private_2" {
+
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private.id
+
+}
+
 resource "aws_subnet" "private_1" {
 
   vpc_id = aws_vpc.this.id
--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -9,3 +9,42 @@ output "vpc_cidr" {
 
   value = aws_vpc.this.cidr_block
 }
+output "public_subnet_1_id" {
+  description = "Public Subnet 1 ID"
+  value       = aws_subnet.public_1.id
+}
+
+output "public_subnet_2_id" {
+  description = "Public Subnet 2 ID"
+  value       = aws_subnet.public_2.id
+}
+
+output "private_subnet_1_id" {
+  description = "Private Subnet 1 ID"
+  value       = aws_subnet.private_1.id
+}
+
+output "private_subnet_2_id" {
+  description = "Private Subnet 2 ID"
+  value       = aws_subnet.private_2.id
+}
+
+output "internet_gateway_id" {
+  description = "Internet Gateway ID"
+  value       = aws_internet_gateway.this.id
+}
+
+output "nat_gateway_id" {
+  description = "NAT Gateway ID"
+  value       = aws_nat_gateway.this.id
+}
+
+output "public_route_table_id" {
+  description = "Public Route Table ID"
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  description = "Private Route Table ID"
+  value       = aws_route_table.private.id
+}
